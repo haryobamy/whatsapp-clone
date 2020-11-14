99f7420a8d97bfe8ece31551f7b385345f82bdf5
haryobamy <haryobamy.badmus@gmail.com>
2020-11-15 00:58:09 +0100

    design of the sidebar header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <div className='app_body'>
+        <Sidebar />
+        {/*chat*/}
+      </div>
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Avatar, IconButton } from '@material-ui/core';
+import { Chat, DonutLarge, MoreVert, Search  } from '@material-ui/icons';
+
+
+import  './Sidebar.css';
+
+
+
+
+function Sidebar() {
+    return (
+        <div>
+            <div className='sidebar'>
+            <div className='sidebar_header'>
+                        <Avatar />  
+                     <div className='sidebar_headerRight'>
+                           <IconButton><DonutLarge /></IconButton> 
+                             <IconButton><Chat /></IconButton>
+                             <IconButton><MoreVert /></IconButton>
+               
+                        </div>
+                    </div>
+
+                    <div className='sidebar_search'>
+                     <div className='sidebar_searchContainer'>
+                     <Search /> 
+                      <input placeholder='search or start new chat' type='text' />
+                     </div>
+                    </div>
+
+                 <div className='sidebar_chats'></div>
+            </div>
+                 
+            
+        </div>
+    )
+}
+
+export default Sidebar;
